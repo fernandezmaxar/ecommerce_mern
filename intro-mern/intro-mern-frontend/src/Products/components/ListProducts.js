68a9824dbaff49579e0d9b5f8999f13470ec9a4a
fernandezmaxar <fernandez.max.ar@gmail.com>
2023-07-06 15:41:13 -0300

    applying environment variables in frontend

--- intro-mern/intro-mern-frontend/src/Products/components/ListProducts.js
+++ intro-mern/intro-mern-frontend/src/Products/components/ListProducts.js
@@ -1,21 +1,7 @@
 import React, { useState, useEffect } from 'react'
+import { getProducts } from '../services'
 import Loading from './Loading'
-import axios from 'axios'
 
-const baseUrl = 'http://localhost:8080/v1'
-
-async function getProducts () {
-    try {
-        const response = await axios({
-            url: `${baseUrl}/products`,
-            method: 'GET'
-        })
-
-        return response
-    } catch (e) {
-        console.log(e)
-    }
-}
 
 const ListProducts = () => {
     const [isLoading, setIsLoading] = useState(true)
@@ -24,12 +10,15 @@ const ListProducts = () => {
     useEffect(() => {
         async function loadProducts() {
             const response = await getProducts()
-            console.log(response)
-            return response
+            
+            if (response.status === 200) {
+                setProducts(response.data.products)
+            }
         }
 
         loadProducts()
-    })
+    }, [])
+
     return (
         isLoading
         ? <Loading />
